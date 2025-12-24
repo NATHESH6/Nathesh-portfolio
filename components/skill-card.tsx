cb8f7ae69398434dae7b133f22fb7fc9a04950a1
NATHESH PV <megaladevi2084@gmail.com>
2025-12-24 08:41:29 +0530

    Update skill-card.tsx

--- components/skill-card.tsx
+++ components/skill-card.tsx
@@ -21,7 +21,7 @@ const getIconUrl = (iconName: string) => {
     mysql: "https://cdn.simpleicons.org/mysql/4479A1",
     vscode: "/vscode-logo.jpg",
     microsoftoffice: "/ms-office-logo.jpg",
-    canva: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTovkB6GVSM8LMlOKFvD8qyiXl9WwtKdo8BYg&s",
+    canva: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgeIxjgyH60tK_BW0mW_HhUE-HAjIJmmlmog&s",
     virtualbox: "https://cdn.simpleicons.org/virtualbox/183A61",
     hardware: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqIYaq3MSCGF5M7A0MQaqNmmx81X5VbHIRvA&s",
   }
