ff42062a4da9fccbb25a437aea57031c12861fb0
NATHESH PV <megaladevi2084@gmail.com>
2026-03-16 12:29:57 +0530

    Remove duplicate certificate entry
    Removed duplicate certificate entry for 'Full Stack Development'.

--- components/sections/certificates.tsx
+++ components/sections/certificates.tsx
@@ -48,12 +48,7 @@ const certificates = [
     date: "December 30th to January 30th 2025-2026 ",
     image: "/Screenshot 2026-02-17 112022.png",
   },
-    {
-    title: "Full Stack Development",
-    issuer: "NoviTech R&D Private Limited",
-    date: "December 30th to January 30th 2025-2026 ",
-    image: "/Screenshot 2026-02-17 112022.png",
-  },
+   
 ]
 
 export default function Certificates() {
